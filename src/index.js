b144b30339d0b6567069816fd89b8943f88c8d45
dppower <powerdp@gmail.com>
2018-07-02 16:35:27 +0100

    Added api and lambda proxy

--- src/index.js
+++ src/index.js
@@ -12,53 +12,26 @@ function CreateChart(settings) {
     })
 }
 
-exports.handler = async () => {
+exports.handler = async (event) => {
     exporter.initPool();
 
-    const chart_settings = {
-        type: 'png',
-        options: {
-            title: {
-                text: 'My Chart'
-            },
-            xAxis: {
-                categories: ["Jan", "Feb", "Mar", "Apr", "Mar", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-            },
-            series: [
-                {
-                    type: 'line',
-                    data: [1, 3, 2, 4]
-                },
-                {
-                    type: 'line',
-                    data: [5, 3, 4, 2]
-                }
-            ]
-        },
-        callback: `function(chart) {
-            chart.renderer.label('This label is added in the callback', 100, 100)
-            .attr({
-                fill : '#90ed7d',
-                padding: 10,
-                r: 10,
-                zIndex: 10
-            })
-            .css({
-                color: 'black',
-                width: '100px'
-            })
-            .add();
-        }`
-    };
+    let chart_settings = JSON.parse(event.body);
 
     try {
         let chart = await CreateChart(chart_settings);
         let data = new Buffer(chart.data, "base64");
-        await s3_client.putObject({ Bucket: process.env.BUCKETNAME, Key: `my-chart.png`, Body: data }).promise();
+        await s3_client.putObject({ Bucket: process.env.BUCKETNAME, Key: `${(chart_settings.options && chart_settings.options.title) || "test-chart"}.png`, Body: data }).promise();
     }
     catch (e) {
         console.log(`error: ${JSON.stringify(e)}`);
+        return {
+            statusCode: 500
+        };
     }
 
     exporter.killPool();
+
+    return {
+        statusCode: 200
+    };
 };
